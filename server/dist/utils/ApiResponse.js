79283f0aaba999f3da2e83d37929d4713bbf1cbe
Ravinder <ravinder729090@gmail.com>
2025-08-05 17:10:27 +0530

    Remove dist and node_module folder from repo

--- server/dist/utils/ApiResponse.js
+++
@@ -1,8 +0,0 @@
-export class ApiResponse {
-    constructor(statusCode = 200, data, message = "Success", success = true) {
-        this.statusCode = statusCode;
-        this.message = message;
-        this.data = data;
-        this.success = success;
-    }
-}
--- server/dist/utils/fileCleanup.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-export const fileCleanup = (localFilePath) => {
-    fs.unlink(localFilePath, () => console.log("Image successful locally deleted"));
-};
